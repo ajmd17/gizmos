882d6257e3824f29ad9307468eb4ef9dab2a79c3
Andrew MacDonald <andrewmacdonald97@hotmail.com>
2016-11-18 15:16:00 -0400

    Update 'cal' and 'clock'

--- clock/clock.js
+++ clock/clock.js
@@ -1,6 +1,12 @@
-function Clock() {
+function Clock(date) {
     this.$clockElement = $("<div>")
         .addClass("clock");
+
+    if (date == undefined) {
+        this.date = new Date();
+    } else {
+        this.date = date;
+    }
 
     this.updateData();
 }
@@ -16,6 +22,11 @@ Clock.prototype.updateData = function() {
     var degStep    = 360 / 12;
     var degCounter = 0;
 
+    var hour = this.date.getHours();
+    var isPm = (hour >= 12);
+    hour = hour % 12;
+    hour = hour != 0 ? hour : 12;
+
     var $hours = $("<ul>")
         .addClass("hour-labels");
 
@@ -25,27 +36,77 @@ Clock.prototype.updateData = function() {
         11, 12, 1, 2
     ];
 
-    this.$clockElement.empty();
-    for (var i = 0; i < 12; i++) {
-        $hours.append($("<li>")
-            .append(hoursList[i])
-            .css({
-                "transform": "rotate(" + degCounter + "deg) translate(" + (halfWidth - 20) + "px) rotate(" + (-1 * degCounter) + "deg)"
-            }));
+    var $am = $("<div>")
+        .append($("<a href=\"#\">")
+        .append("AM"))
+        .click(function() {
+            clock.$clockElement.find(".am-pm>div").removeClass("active");
+            $(this).addClass("active");
+        });
 
+    var $pm = $("<div>")
+        .append($("<a href=\"#\">")
+        .append("PM"))
+        .click(function() {
+            clock.$clockElement.find(".am-pm>div").removeClass("active");
+            $(this).addClass("active");
+        });
+
+    if (isPm) {
+        $pm.addClass("active");
+    } else {
+        $am.addClass("active");
+    }
+
+    this.$clockElement.empty();
+    this.$clockElement.append($("<div>")
+        .addClass("clock-controls")
+        .append($("<div>")
+            .addClass("hours-hand"))
+        .append($("<div>")
+            .addClass("center-cap"))
+        .append($("<div>")
+            .addClass("am-pm")
+            .append($am)
+            .append($pm)));
+
+    
+
+    for (var i = 0; i < 12; i++) {
+
+        (function(hourIndex) {
+            var deg = (hoursList[hourIndex] / 12) * 360;
+
+            var $hourElement = $("<li>")
+                .addClass("hour")
+                .append($("<a href=\"#\">")
+                    .append(hoursList[hourIndex]))
+                .css({
+                    "transform": "rotate(" + degCounter + "deg) translate(" + (halfWidth - 30) + "px) rotate(" + (-1 * degCounter) + "deg)"
+                })
+                .click(function() {
+                    clock.$clockElement.find(".hour").removeClass("active");
+                    $(this).addClass("active");
+
+                    // rotate hour hand to face this
+                    clock.$clockElement.find(".hours-hand").css({
+                        "transform": "translateX(-50%) translateY(-50%) rotate(" + deg.toString() + "deg)"
+                    });
+                });
+
+            if (hoursList[hourIndex] == hour) {
+                $hourElement.addClass("active");
+                console.log(clock.$clockElement.find(".hours-hand"));
+                clock.$clockElement.find(".hours-hand").css({
+                    "transform": "translateX(-50%) translateY(-50%) rotate(" + deg.toString() + "deg)"
+                });
+            }
+
+            $hours.append($hourElement);
+        })(i);
+    
         degCounter += degStep;
     }
 
     this.$clockElement.append($hours);
-    this.$clockElement.append($("<div>")
-        .addClass("clock-controls")
-        .append($("<div>")
-            .addClass("hours-hand")
-            /*.css({
-                "transform": "rotate(" + 260 + "deg) translateX(" + (halfWidth - 130) + "px) translateY(" + (halfWidth - 130) + "px)"
-            })*/)
-        .append($("<div>")
-            .addClass("minutes-hand"))
-        .append($("<div>")
-            .addClass("center-cap")));
 };
